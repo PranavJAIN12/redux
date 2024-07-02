d3a9de06535fc58f04a4dac6f925f40da9243c9c
PranavJAIN12 <94856533+PranavJAIN12@users.noreply.github.com>
2024-07-02 23:21:52 +0530

    file added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './Navbar';
+import Data from './Data';
 
 function App() {
   return (
    <>
     <Navbar/>
+    <Data/>
    </>
   );
 }
---
+++ src/Data.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function Data() {
+  return (
+    <div className='container'>
+    <h1>Deposit/Withdraw Money  </h1><br/>
+    <div className="addcart" style={{display:'flex', flexDirection:'row'}}>
+    <button className="btn btn-primary mx-3">+</button>
+    <p>Add to cart</p>
+    <button className='btn btn-primary mx-3'>-</button>
+    </div>
+    
+      
+    </div>
+  )
+}
+
+
+
